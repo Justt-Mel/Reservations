10b87c24aecac00238d68f203585ea5a1199cd25
Justt-Mel <justinmelendezj@gmail.com>
2025-06-05 04:02:58 -0400

    finished making functions for the tables for customers, restaurants, and reservations

--- resDb.js
+++ resDb.js
@@ -13,6 +13,26 @@ const newCustomer = async (customer)=>{
     return response.rows[0]
 }
 
+const newRestaurant = async (restaurant)=> {
+    const SQL = `
+    INSERT INTO restaurants (id,name)
+    VALUES ($1, $2)
+    RETURNING *
+    `
+    const response = await client.query(SQL, [uuidv4(), restaurant.name])
+    return response.rows[0]
+}
+
+const newReservation = async (reservation) =>{
+    const SQL = `
+    INSERT INTO reservations (id, customers_id, restaurants_id,party_size)
+    VALUES ($1,$2,$3,$4)
+    RETURNING *
+    `
+    const response = await client.query(SQL, [uuidv4(), reservation.customers_id, reservation.restaurants_id, reservation.party_size])
+    return response.rows[0]
+}
+
 const seed =async ()=>{
     const SQL =`
     DROP TABLE IF EXISTS reservations;
@@ -21,8 +41,7 @@ const seed =async ()=>{
 
     CREATE TABLE customers(
     id  UUID PRIMARY KEY,
-    name VARCHAR(100),
-    party_size INT
+    name VARCHAR(100)
     );
     CREATE TABLE restaurants(
     id UUID PRIMARY KEY,
@@ -32,18 +51,36 @@ const seed =async ()=>{
     id UUID PRIMARY KEY, 
     customers_id UUID REFERENCES customers(id) NOT NULL,
     restaurants_id UUID REFERENCES restaurants(id) NOT NULL,
+    party_size INT,
     created_at TIMESTAMP DEFAULT now()
     );
     `
     await client.query(SQL)
     console.log("TABLES CREATED")
-    await Promise.all([
+    const [Josh, Cassandra, Melissa, Pedro, Tom] = await Promise.all([
         newCustomer({name:'Josh'}),
         newCustomer({name:'Cassandra'}),
         newCustomer({name:'Melissa'}),
         newCustomer({name:'Pedro'}),
         newCustomer({name:'Tom'})
     ])
+
+    const [pizzaSpot, Meraluna, Vincenzo, JinRamen, jacobsPickle] = await Promise.all([
+        newRestaurant({name:'Pizza Spot'}),
+        newRestaurant({name:'Meraluna'}),
+        newRestaurant({name:'Vincenzo'}),
+        newRestaurant({name:'Jin Ramen'}),
+        newRestaurant({name:'jacobs Pickle'})
+    ])
+
+    await Promise.all([
+        newReservation({customers_id: Cassandra.id, restaurants_id: pizzaSpot.id, party_size: 4}),
+        newReservation({customers_id: Pedro.id, restaurants_id: jacobsPickle.id, party_size: 3}),
+        newReservation({customers_id: Josh.id, restaurants_id: JinRamen.id, party_size: 5}),
+        newReservation({customers_id: Melissa.id, restaurants_id: Meraluna.id, party_size: 1}),
+        newReservation({customers_id: Tom.id, restaurants_id: Vincenzo.id, party_size: 7})
+    ])
+
     console.log("table seeded")
 
 }
